6735dcb9541cdcd3ab411a17f2111cb0aadb5e49
Juan Rafael Ibáñez García <jribanezgarcia@gmail.com>
2026-05-02 14:09:42 +0200

    Interfaz grafica Usuarios totalmente funcional. Permite alta, baja y modificacion. Buscar. Implementada la clase Dialogos para los mensajes varios.

--- src/main/java/biblioteca/vista/controlador/FormularioUsuarioController.java
+++ src/main/java/biblioteca/vista/controlador/FormularioUsuarioController.java
@@ -1,13 +1,25 @@
 package biblioteca.vista.controlador;
 
+import biblioteca.modelo.dominio.Direccion;
+import biblioteca.modelo.dominio.Usuario;
+import biblioteca.utilidades.Dialogos;
+import biblioteca.vista.Vista;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.util.List;
+import javafx.event.ActionEvent;
 
 public class FormularioUsuarioController {
 
     @FXML
     private Button botonGuardarUsuario;
+
+    @FXML
+    private Label labelUsuario;
 
     @FXML
     private TextField txtCodigoPostal;
@@ -29,5 +41,84 @@ public class FormularioUsuarioController {
 
     @FXML
     private TextField txtVia;
+
+    private List<Usuario> listaUsuarios;
+
+    private Usuario registro;
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios){
+        this.listaUsuarios = listaUsuarios;
+    }
+    public void setRegistro(Usuario u){
+        this.registro = u;
+        if(this.registro!=null){
+            labelUsuario.setText("Editar Usuario");
+            this.txtDni.setText(this.registro.getDni());
+            this.txtEmail.setText(this.registro.getEmail());
+            this.txtLocalidad.setText(this.registro.getLocalidad());
+            this.txtNombre.setText(this.registro.getNombre());
+            this.txtNumero.setText(this.registro.getNumero());
+            this.txtVia.setText(this.registro.getVia());
+            this.txtCodigoPostal.setText(this.registro.getCp());
+        }else{
+            labelUsuario.setText("Añadir Usuario");
+        }
+    }
+    public Usuario getRegistro(){
+        return this.registro;
+    }
+    @FXML
+    void GuardarUsuario(ActionEvent event){
+        try{
+            String dni= this.txtDni.getText();
+            String nombre=this.txtNombre.getText();
+            String email=this.txtEmail.getText();
+            String via=this.txtVia.getText();
+            String numero=this.txtNumero.getText();
+            String cp=this.txtCodigoPostal.getText();
+            String localidad=this.txtLocalidad.getText();
+
+            if(this.registro==null){
+                Direccion direccion = new Direccion(via,numero,cp,localidad);
+                Usuario u= new Usuario(dni,nombre,email,direccion);
+                if(this.listaUsuarios.contains(u)==false){
+                    //Añadimos el usuario a la base de datos.
+                    Vista.getInstancia().getControlador().alta(u);
+                    Dialogos.mostrarDialogoAdvertencia("Mensaje Usuario","Usuario añadido correctamente");
+                    this.registro= u;
+                    Stage escenarioActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    escenarioActual.close();
+                }else{
+                    Dialogos.mostrarDialogoAdvertencia("Mensaje Usuario","El Usuario ya existe");
+                }
+            //modificamos un usuario existente
+            }else{
+                /*this.registro.setDni(dni);
+                this.registro.setNombre(nombre);
+                this.registro.setEmail(email);
+                this.registro.setDireccion(new Direccion(via,numero,cp,localidad));*/
+                //prueba "modificar" base de datos , en realdiad es baja y alta no update
+                Direccion direccion = new Direccion(via, numero, cp, localidad);
+                Usuario uNuevo = new Usuario(dni, nombre, email, direccion);
+                // this.registro sigue siendo el usuario ORIGINAL (con el DNI antiguo)
+                Vista.getInstancia().getControlador().baja(this.registro);
+                Vista.getInstancia().getControlador().alta(uNuevo);
+
+                Dialogos.mostrarDialogoAdvertencia("Mensaje Usuario","Usuario MODIFICADO correctamente");
+                Stage escenarioActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                escenarioActual.close();
+            }
+
+
+        }catch (NumberFormatException e){
+            Dialogos.mostrarDialogoError("Error Guardar Usuario", "Tiene que ser un numero ese campo");
+        } catch (Exception e) {
+            Dialogos.mostrarDialogoError("Error Guardar Usuario", e.getMessage());
+        }
+
+    }
+
+
+
 
 }
--- src/main/java/biblioteca/vista/controlador/UsuariosController.java
+++ src/main/java/biblioteca/vista/controlador/UsuariosController.java
@@ -1,6 +1,7 @@
 package biblioteca.vista.controlador;
 
 import biblioteca.modelo.dominio.Usuario;
+import biblioteca.utilidades.Dialogos;
 import biblioteca.vista.Vista;
 import biblioteca.vista.recursos.LocalizadorRecursos;
 import javafx.collections.FXCollections;
@@ -17,6 +18,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -62,21 +64,28 @@ public class UsuariosController implements Initializable {
     @FXML
     private TableView<Usuario> tablaUsuarios;
     List<Usuario> listaUsuarios;
-    private ObservableList<Usuario> ListaUsuariosVisible;
+    private ObservableList<Usuario> listaUsuariosVisible;
     private Usuario registro;
     private String filtro;
 
+
     @FXML
-    void cargarFormularioUsuarioController(ActionEvent event) throws IOException {
+    void addUsuario(ActionEvent event) throws Exception {
         FXMLLoader fxmlLoader=new FXMLLoader(LocalizadorRecursos.class.getResource("FormularioUsuario.fxml"));
         Parent raiz = fxmlLoader.load();
         Scene escenaAMUsuaios = new Scene(raiz);
+        //pasamos el controlador a la nueva ventana
+        FormularioUsuarioController cF= fxmlLoader.getController();
+        cF.setListaUsuarios(this.listaUsuarios);
+        cF.setRegistro(null);
         Stage escenarioAMUsuarios= new Stage();
         escenarioAMUsuarios.initModality(Modality.APPLICATION_MODAL);
         escenarioAMUsuarios.setTitle("Formulario Usuario");
         escenarioAMUsuarios.setScene(escenaAMUsuaios);
         escenarioAMUsuarios.setResizable(false);
         escenarioAMUsuarios.showAndWait();
+        //refrescamos la tabla despues de añadir un usuario
+        this.refrescarTabla();
 
     }
 
@@ -91,15 +100,18 @@ public class UsuariosController implements Initializable {
         this.colCp.setCellValueFactory(new PropertyValueFactory<Usuario,String>("cp"));
         this.colLocalidad.setCellValueFactory(new PropertyValueFactory<Usuario,String>("localidad"));
         this.filtro ="";
-        ListaUsuariosVisible= FXCollections.observableArrayList();
-        this.tablaUsuarios.setItems(ListaUsuariosVisible);
+        //creamos la tabla visible
+        listaUsuariosVisible= FXCollections.observableArrayList();
+        //se la pasamos a la tabla
+        this.tablaUsuarios.setItems(listaUsuariosVisible);
         //tengo que hacer un try catch para manejar la excepcion puesto que inizialice no deja hacer throws
+        //cargamos la lista de la BD
         try {
             this.listaUsuarios= Vista.getInstancia().getControlador().listadoUsuario();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        //refrescamos la tabla para cargarla al inicio.
+        //refrescamos la tabla para cargarla pasando los datos de listaUsuarios a ListaUsuariosVisible
         try {
             this.refrescarTabla();
         } catch (Exception e) {
@@ -109,21 +121,74 @@ public class UsuariosController implements Initializable {
 
     }
     @FXML
-    void Buscar(KeyEvent event) throws Exception {
+    void BuscarUsuario(KeyEvent event) throws Exception {
         this.filtro = this.campoBuscarUsuario.getText();
         this.refrescarTabla();
     }
+    @FXML
+    void BorrarUsuario(ActionEvent event) throws Exception {
 
+        if(this.registro==null){
+            Dialogos.mostrarDialogoInformacion("Error","No has seleccionado ningun Usuario");
+        }else{
+            try{
+                Vista.getInstancia().getControlador().baja(this.registro);
+                Dialogos.mostrarDialogoInformacion("Borrar Usuario","Usuario borrado correctamente");
+            }catch(Exception e){
+
+            }
+            this.refrescarTabla();
+        }
+    }
+    @FXML
+    void EditarUsuario(ActionEvent event){
+
+        if(this.registro==null){
+            Dialogos.mostrarDialogoAdvertencia("ERROR Editar Usuario","No has seleccionado ningun Usuario");
+        }else{
+            try{
+                FXMLLoader fxmlLoader = new FXMLLoader(LocalizadorRecursos.class.getResource("FormularioUsuario.fxml"));
+                Parent raiz = fxmlLoader.load();
+                Scene escena = new Scene(raiz);
+
+                FormularioUsuarioController cF= fxmlLoader.getController();
+                //pasamos la list de usuarios al formulario
+                cF.setListaUsuarios(this.listaUsuarios);
+                //pasamos el usuario seleccionado al formulario
+                cF.setRegistro(this.registro);
+
+                Stage nuevoEscenario=new Stage();
+                nuevoEscenario.initModality(Modality.APPLICATION_MODAL);
+                nuevoEscenario.setTitle("Editar persona...");
+                nuevoEscenario.setScene(escena);
+                nuevoEscenario.showAndWait();
+                //refrescamos solo la tabla de la vista, porque solo se modifico el mismo objeto no se ha modificado la DB.
+                //Esto lo he hecho asi porque Usuario no tiene metodo Modificar.
+                //this.tablaUsuarios.refresh();
+                //refrescamos la tabla con ls modificaciones de la BD.
+                this.refrescarTabla();
+
+            } catch (Exception e) {
+                Dialogos.mostrarDialogoError("ERROR",e.getMessage());
+            }
+        }
+    }
+    @FXML
+    void SeleccionarUsuario(MouseEvent event){
+        this.registro = this.tablaUsuarios.getSelectionModel().getSelectedItem();
+    }
+
+    //metodo para pasar el array que viene de la BD a la ObservableTable
     private void refrescarTabla() throws Exception {
         this.registro = null;
         this.tablaUsuarios.getSelectionModel().clearSelection();
 
         this.listaUsuarios = Vista.getInstancia().getControlador().listadoUsuario();
-        this.ListaUsuariosVisible.clear();
+        this.listaUsuariosVisible.clear();//borramos lo que se ve
 
         for(Usuario u : this.listaUsuarios) {
             if(this.filtro.isEmpty() || u.getNombre().toLowerCase().contains(this.filtro.toLowerCase())) {
-                this.ListaUsuariosVisible.add(u);
+                this.listaUsuariosVisible.add(u);
             }
         }
     }
